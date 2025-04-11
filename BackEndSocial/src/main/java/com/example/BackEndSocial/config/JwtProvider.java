2efad5c2d788463d29f264fbcfb3a723bfa1466d
muei <phanthanhtu9a5bl2017@gmail.com>
2025-04-11 17:10:10 +0700

    update

--- BackEndSocial/src/main/java/com/example/BackEndSocial/config/JwtProvider.java
+++ BackEndSocial/src/main/java/com/example/BackEndSocial/config/JwtProvider.java
@@ -17,7 +17,7 @@ public class JwtProvider {
         Collection<? extends GrantedAuthority> authorities= auth.getAuthorities();
         String roles = populateAuthorities(authorities);
         String jwt= Jwts.builder().setIssuedAt(new Date())
-                .setExpiration((new Date(new Date().getTime()+864000)))
+                .setExpiration((new Date(new Date().getTime()+7 * 24 * 60 * 60 * 1000)))
                 .claim("email", auth.getName())
                 .claim("authorities", roles)
                 .signWith(key)
