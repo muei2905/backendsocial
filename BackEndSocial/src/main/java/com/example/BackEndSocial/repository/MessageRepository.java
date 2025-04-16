fb7969fd87a440852d5e4942f72f07228c3249d2
muei <phanthanhtu9a5bl2017@gmail.com>
2025-04-16 10:53:33 +0700

    update

--- BackEndSocial/src/main/java/com/example/BackEndSocial/repository/MessageRepository.java
+++ BackEndSocial/src/main/java/com/example/BackEndSocial/repository/MessageRepository.java
@@ -21,7 +21,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query("SELECT m FROM Message m WHERE " +
             "((m.sender.id = :userId1 AND m.receiver.id = :userId2) OR " +
-            " (m.sender.id = :userId2 AND m.receiver.id = :userId1)) AND " +
+            " (m.sender.id = :userId2 AND m.receiver.id = :userId1)) " +
             "ORDER BY m.timestamp DESC")
     Page<Message> findMessagesBetweenPaged(Long userId1, Long userId2, Pageable pageable);
 
