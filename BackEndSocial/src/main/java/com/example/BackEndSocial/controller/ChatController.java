3f82fda56cd376a21a033696965fb4141a728687
muei <phanthanhtu9a5bl2017@gmail.com>
2025-04-23 15:55:12 +0700

    gi v tr

--- BackEndSocial/src/main/java/com/example/BackEndSocial/controller/ChatController.java
+++ BackEndSocial/src/main/java/com/example/BackEndSocial/controller/ChatController.java
@@ -35,29 +35,29 @@ public class ChatController {
         return message;
     }
 
-    @MessageMapping("/chat")
+    @MessageMapping("/chat/read")
     public void markMessageAsRead(@Payload Map<String, Long> payload, Principal principal) {
         Long messageId = payload.get("messageId");
         messageService.markAsRead(messageId);
 
         MessageDTO msg = messageService.getMessageById(messageId);
         messagingTemplate.convertAndSendToUser(
-                String.valueOf(msg.getSenderId()), "/queue/read", msg
+                String.valueOf(msg.getSenderId()), "/queue", msg
         );
         messagingTemplate.convertAndSendToUser(
-                String.valueOf(msg.getReceiverId()), "/queue/read", msg
+                String.valueOf(msg.getReceiverId()), "/queue", msg
         );
     }
-    @MessageMapping("/chat")
+    @MessageMapping("/chat/delete")
     public void deleteMessage(@Payload Map<String, Long> payload, Principal principal) throws Exception {
         Long messageId = payload.get("messageId");
         Message deletedMessage = messageService.deleteMessage(messageId, principal.getName());
 
         messagingTemplate.convertAndSendToUser(
-                String.valueOf(deletedMessage.getSender().getId()), "/queue/delete", deletedMessage
+                String.valueOf(deletedMessage.getSender().getId()), "/queue", deletedMessage
         );
         messagingTemplate.convertAndSendToUser(
-                String.valueOf(deletedMessage.getReceiver().getId()), "/queue/delete", deletedMessage
+                String.valueOf(deletedMessage.getReceiver().getId()), "/queue", deletedMessage
         );
     }
 
